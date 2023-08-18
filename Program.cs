a7de789de39d286e7a4f679b55f1408fa179e1cd
Shmak <shizzz777@yandex.ru>
2023-08-18 11:43:57 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int Prompt (string msg)
+{
+
+System.Console.WriteLine(msg);
+string value = Console.ReadLine()
+int result = Console.ToInt32(value);
+return result;
+}
+
+
+int number = Prompt("Введите трехзначное число> ");
+if (number < 100 || number >= 1000)
+{
+
+Console.WriteLine ("Вы ввели не трехзначное число");
+    return;
+}
+
+
+Console.WriteLine($" число '{number}'");
+int secondRank = number / 10 % 10 ;
+Console.WriteLine($"Вторая цифра'{secondRank}'");
